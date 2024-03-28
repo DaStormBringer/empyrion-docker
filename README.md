9d0a21391128f38504b72981a4b22c6d36173044
DaStormBringer <chad@shipman-clan.com>
2024-03-28 18:22:25 -0400

    Initial Changes to Load RE2 and some changes

--- README.md
+++ README.md
@@ -6,12 +6,16 @@ The image itself contains WINE and steamcmd, along with an entrypoint.sh script 
 When running the image, mount the volume /home/user/Steam, to persist the Empyrion install and avoid downloading it on each container start.
 Sample invocation:
 ```
+cd empyrion-docker
+docker build -t empyrion-dedicated-server .
 mkdir -p gamedir
-docker run -di -p 30000:30000/udp --restart unless-stopped -v $PWD/gamedir:/home/user/Steam bitr/empyrion-server
+docker run -di -p 30000:30000/udp -p 30001:30001/udp --restart unless-stopped -v $PWD/gamedir:/home/user/Steam empyrion-dedicated-server
 
 # for experimental version:
+cd empyrion-docker
+docker build -t empyrion-dedicated-server .
 mkdir -p gamedir_beta
-docker run -di -p 30000:30000/udp --restart unless-stopped -v $PWD/gamedir_beta:/home/user/Steam -e BETA=1 bitr/empyrion-server
+docker run -di -p 30000:30000/udp --restart unless-stopped -v $PWD/gamedir_beta:/home/user/Steam -e BETA=1 empyrion-dedicated-server
 ```
 
 After starting the server, you can edit the dedicated.yaml file at 'gamedir/steamapps/common/Empyrion - Dedicated Server/dedicated.yaml'.
@@ -26,4 +30,4 @@ docker run -di -p 30000:30000/udp --restart unless-stopped -v $PWD/gamedir:/home
 
 To append arguments to the steamcmd command, use `-e "STEAMCMD=..."`. Example: `-e "STEAMCMD=+runscript /home/user/Steam/addmods.txt"`.
 
-For more information about the dedicated server itself, refer to the [wiki](https://empyrion.gamepedia.com/Dedicated_Server_Setup).
+For more information about the dedicated server itself, refer to the [wiki](https://empyrion.fandom.com/wiki/Guide/Setting_Up_Dedicated_Server).
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/bash -ex
 #Notes: steamcmd +login anonymous +force_install_dir c:\Empyrion +app_update 530870 validate +quit
 ##Empyrion AppID 383120
-##RE 1.10 2918811239
+##Empyrion Dedicated Server AppID 530870
+##RE 1.11 2918811239
+##RE 2 (Alpha) 3143225812
 [ "$UID" != 0 ] || {
     mkdir -p ~user/Steam
     chown user: ~user/Steam
@@ -12,30 +14,30 @@
 GAMEDIR="$HOME/Steam/steamapps/common/Empyrion - Dedicated Server/DedicatedServer"
 
 cd "$HOME"
-STEAMCMD="./steamcmd.sh +@sSteamCmdForcePlatformType windows +login anonymous $STEAMCMD"
+STEAMCMD="./steamcmd.sh +@sSteamCmdForcePlatformType windows +login anonymous $STEAMCMD +app_update 530870"
+
+# add beta phase (must be set after app_update)
 [ -z "$BETA" ] || STEAMCMD="$STEAMCMD -beta experimental"
 
 # eval to support quotes in $STEAMCMD
-eval "$STEAMCMD +app_update 530870 +quit"
+eval "$STEAMCMD +quit"
 
-CLONEDIR="/home/user/Steam/steamapps/common/Empyrion - Dedicated Server/Content/Scenarios"
-REPO_URL="https://github.com/xSevithx/Empyrion-REFiles.git"
-REPO_DIR="ReforgedEden"
+SCENARIOS_DIR="/home/user/Steam/steamapps/common/Empyrion - Dedicated Server/Content/Scenarios"
+RE_DIR="ReforgedEden2"
 
 # Check if the repository directory already exists
-if [ ! -d "$CLONEDIR/$REPO_DIR" ]; then
-    cd "$CLONEDIR"
-    git clone "$REPO_URL" "$REPO_DIR"
+if [ ! -d "$SCENARIOS_DIR/$REPO_DIR" ]; then
+    cd "$SCENARIOS_DIR"
+   #  SOmething something maybe download and unzip
 else
-    echo "Repository directory '$REPO_DIR' already exists. Skipping clone."
+    echo "Scenarios directory '$RE_DIR' already exists. Skipping clone."
 fi
 
-#End RE Clone
 
 mkdir -p "$GAMEDIR/Logs"
 
 rm -f /tmp/.X1-lock
-Xvfb :1 -screen 0 800x600x24 &
+Xvfb :1 -screen 0 800x600x24 -nolisten unix &
 export WINEDLLOVERRIDES="mscoree,mshtml="
 export DISPLAY=:1
 
@@ -47,4 +49,4 @@ sh -c 'until [ "`netstat -ntl | tail -n+3`" ]; do sleep 1; done
 sleep 5 # gotta wait for it to open a logfile
 tail -F Logs/current.log ../Logs/*/*.log 2>/dev/null' &
 
-/opt/wine-staging/bin/wine ./EmpyrionDedicated.exe -batchmode -nographics -dedicated /dedicated.yaml -logFile Logs/current.log "$@" &> Logs/wine.log
+/opt/wine-staging/bin/wine ./EmpyrionDedicated.exe -batchmode -nographics -dedicated /dedicated_custom.yaml -logFile Logs/current.log "$@" &> Logs/wine.log
